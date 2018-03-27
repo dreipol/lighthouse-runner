10e06a823db0c6a24c81db8c4f82541dfc226483
fabs <faebeee@gmail.com>
2018-03-27 21:42:46 +0200

    New build

--- dist/CLI/index.js
+++ dist/CLI/index.js
@@ -2,7 +2,7 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 const NoopResultPersister_1 = __importDefault(require("../Runner/ResultPersister/NoopResultPersister"));
 const Runner_1 = require("../Runner");
