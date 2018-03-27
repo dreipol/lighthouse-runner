91360af88f29685ff876d362e23987b9ff62b1f3
fabs <faebeee@gmail.com>
2018-03-27 19:36:13 +0200

    new build

--- dist/CLI/commands/setup.js
+++ dist/CLI/commands/setup.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 }
@@ -14,6 +22,8 @@ exports.default = ({
         }
     },
     handler(argv) {
-        writeDefaultConfig_js_1.default(argv.config);
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield writeDefaultConfig_js_1.default(argv.config);
+        });
     }
 });
