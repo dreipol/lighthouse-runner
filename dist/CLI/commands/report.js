b46db95b982630b31aeb9fad9a451e47aed5210c
fabs <faebeee@gmail.com>
2018-03-27 20:54:24 +0200

    New build

--- dist/CLI/commands/report.js
+++ dist/CLI/commands/report.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("../../Runner/index");
 const ConsoleLogger_1 = __importDefault(require("../../Runner/Logger/ConsoleLogger"));
