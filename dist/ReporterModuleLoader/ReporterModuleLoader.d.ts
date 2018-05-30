5eaadafae3cae6c0cff038ac32bc809b310f07fd
fabs <faebeee@gmail.com>
2018-05-30 14:09:42 +0200

    Cleanup build

--- dist/ReporterModuleLoader/ReporterModuleLoader.d.ts
+++
@@ -1,7 +0,0 @@
-import AbstractResultReporter from '../ResultReporter/AbstractResultReporter';
-import ResultReporterInterface from '../ResultReporter/ResultReporterInterface';
-import { DreihouseConfigInterface, LoggerInterface } from '@dreipol/lighthouse-config';
-export default class ReporterModuleLoader {
-    static load(reportFolder: string | null, config: DreihouseConfigInterface, logger: LoggerInterface, loaders: Array<string | ResultReporterInterface>): ResultReporterInterface[];
-    protected static getMappedReporter(key: string): new (...args: any[]) => AbstractResultReporter;
-}
--- dist/ResultReporter/ResultReporterModuleLoader.d.ts
+++
@@ -1,7 +0,0 @@
-import AbstractResultReporter from './AbstractResultReporter';
-import ResultReporterInterface from './ResultReporterInterface';
-import { DreihouseConfigInterface, LoggerInterface } from '../../../lighthouse-config/dist/index';
-export default class ResultReporterModuleLoader {
-    static load(reportFolder: string | null, config: DreihouseConfigInterface, logger: LoggerInterface, loaders: Array<string | ResultReporterInterface>): ResultReporterInterface[];
-    protected static getMappedReporter(key: string): new (...args: any[]) => AbstractResultReporter;
-}
